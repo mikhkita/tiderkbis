7f302e652c6164d79189b4a6c16c8cc72e326ac5
Китаев Михаил <beatbox787@gmail.com>
2019-07-13 18:48:16 +0700

    no message

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<div class="b-rating">
+	<div class="b-block">
+		<h3>Помогите нам стать лучше</h3>
+		<p>Оцените нашу новую страницу</p>
+		<div class="b-stars">
+			<div class="b-star selected"></div>
+			<div class="b-star selected"></div>
+			<div class="b-star selected"></div>
+			<div class="b-star"></div>
+			<div class="b-star"></div>
+		</div>
+	</div>
+</div>
 <div class="b b-footer">
 	<div class="b-block">
 		
