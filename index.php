f30f836d7414d9cabcd55696bae0b1a1e72c3c35
Китаев Михаил <beatbox787@gmail.com>
2019-07-13 18:37:18 +0700

    no message

--- index.php
+++ index.php
@@ -18,40 +18,8 @@
 	<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
 </head>
 <body>
-	<div class="b b-header">
-		<div class="b-grey-header clearfix">
-			<div class="b-block clearfix">
-				<a href="index.php" class="b-logo"><img src="i/logo.svg" alt=""></a>
-				<a href="#" class="b-city-btn">
-					<span class="b-city-btn-icon"></span><span class="b-city-btn-text">Томск</span><span class="b-city-btn-arrow"></span>
-				</a>
-				<a href="tel:83822513614" class="b-phone"><span class="b-phone-icon"></span>(3822) 51-36-14</a>
-				<div class="b-header-right">
-					<a href="#" class="b-personal-btn">
-						<span class="b-personal-btn-icon"></span><span class="b-personal-btn-text">Личный кабинет</span>
-					</a>
-					<a href="#" class="b-search-btn">
-						<span class="b-search-btn-icon"></span><span class="b-search-btn-text">Поиск</span>
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="b-white-header">
-			<div class="b-block clearfix">
-				<ul class="b-main-menu justify">
-					<li><a href="#">Займы</a></li>
-					<li><a href="#">Сбережения</a></li>
-					<li><a href="#">Погашение займа</a></li>
-					<li><a href="#">О кооперативе</a></li>
-					<li><a href="#">Онлайн-приёмная</a></li>
-					<li><a href="#">Контакты</a></li>
-				</ul>
-				<a href="#" class="b-menu-btn">
-					<span class="b-menu-btn-icon"></span>
-				</a>
-			</div>
-		</div>
-	</div>
+	<? include "header.php"; ?>
+
 	<div class="b-main-slider">
 		<div class="b-slider-item" style="background-image: url('i/main-back.jpg');">
 			<div class="b-block">
@@ -73,7 +41,7 @@
 
 
 
-
+	<? include "footer.php"; ?>
 
 	<div style="display:none;">
 		<a href="#b-popup-error" class="b-error-link fancy" style="display:none;"></a>
