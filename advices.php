1149ab3398a2d6cb7bf7cfcfec9a79028d3e61ab
Китаев Михаил <beatbox787@gmail.com>
2019-07-14 13:00:10 +0700

    no message

--- advices.php
+++ advices.php
@@ -4,22 +4,28 @@
 			<h3>Полезные советы</h3>
 			<a href="#" class="b-small-btn">Все советы</a>
 		</div>
-		<ul class="b-advice-list">
-			<li class="b-advice">
+		<div class="b-advice-list">
+			<a href="#" class="b-advice">
 				<div class="b-img" style="background-image: url('i/advice-1.jpg');"></div>
-				<h4>Как получить льготные условия</h4>
-				<a href="#" class="b-green-link">Подробнее</a>
-			</li>
-			<li class="b-advice">
-				<div class="b-img" style="background-image: url('i/advice-1.jpg');"></div>
-				<h4>Как упростить процесс перечисления платежей по займам КПКГ</h4>
-				<a href="#" class="b-green-link">Подробнее</a>
-			</li>
-			<li class="b-advice">
-				<div class="b-img" style="background-image: url('i/advice-1.jpg');"></div>
-				<h4>Как лучше погасить задолженность по займу</h4>
-				<a href="#" class="b-green-link">Подробнее</a>
-			</li>
-		</ul>
+				<div class="b-advice-text">
+					<h4>Как получить  льготные условия</h4>
+					<div class="b-green-link">Подробнее<span class="right-arrow-icon"></span></div>
+				</div>
+			</a>
+			<a href="#" class="b-advice">
+				<div class="b-img" style="background-image: url('i/advice-2.jpg');"></div>
+				<div class="b-advice-text">
+					<h4>Как упростить процесс перечисления платежей по займам КПКГ</h4>
+					<div class="b-green-link">Подробнее<span class="right-arrow-icon"></span></div>
+				</div>
+			</a>
+			<a href="#" class="b-advice">
+				<div class="b-img" style="background-image: url('i/advice-3.jpg');"></div>
+				<div class="b-advice-text">
+					<h4>Как лучше погасить задолженность по займу</h4>
+					<div class="b-green-link">Подробнее<span class="right-arrow-icon"></span></div>
+				</div>
+			</a>
+		</div>
 	</div>
 </div>
