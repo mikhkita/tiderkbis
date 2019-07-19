29316c97b2286914fd8c2a2bdb908903a8550647
Дмитрий Муленок <mulenokd@gmail.com>
2019-07-19 14:15:18 +0700

    no message

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" href="css/KitAnimate.css" type="text/css">
 	<link rel="stylesheet" href="css/slick.css" type="text/css">
 	<link rel="stylesheet" href="css/chosen.min.css" type="text/css">
+	<link rel="stylesheet" href="css/datepicker.min.css" type="text/css">
 	<link rel="stylesheet" href="css/base.css" type="text/css">
 	<link rel="stylesheet" href="css/layout.css" type="text/css">
 
