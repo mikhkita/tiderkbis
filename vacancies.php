d8fa4999d1dd54440e05a224aa4d6944b709e5a8
Китаев Михаил <beatbox787@gmail.com>
2019-07-20 13:19:43 +0700

    no message

---
+++ vacancies.php
@@ -0,0 +1,48 @@
+<? 
+
+$params = array(
+	"TITLE" => "Вакансии",
+	"BREADCRUMBS" => array(
+		"Главная" => "index.php",
+	),
+);
+
+include "header.php";
+
+?>
+	
+<div class="b-vacancy-content">
+	<div class="b-block clearfix">
+		<div class="b-left-block">
+			<a href="#" class="b-white-tile b-vacancy-tile">
+				<div class="b-city-btn">
+					<span class="b-city-btn-icon"></span><span class="b-city-btn-text">Барабинск</span>
+				</div>
+				<h4>Оператор колл-центра</h4>
+				<div class="grey-text b-vacancy-section">Поддержка, продажи</div>
+				<div class="b-vacancy-skill">Без опыта</div>
+			</a>
+			<a href="#" class="b-white-tile b-vacancy-tile">
+				<div class="b-city-btn">
+					<span class="b-city-btn-icon"></span><span class="b-city-btn-text">Томск</span>
+				</div>
+				<h4>Сотрудник отделения</h4>
+				<div class="grey-text b-vacancy-section">Поддержка, продажи</div>
+				<div class="b-vacancy-skill">Без опыта</div>
+			</a>
+			<a href="#" class="b-white-tile b-vacancy-tile">
+				<div class="b-city-btn">
+					<span class="b-city-btn-icon"></span><span class="b-city-btn-text">Томск</span>
+				</div>
+				<h4>Управляющий отделом</h4>
+				<div class="grey-text b-vacancy-section">Поддержка, продажи</div>
+				<div class="b-vacancy-skill"></div>
+			</a>
+		</div>
+		<div class="b-right-block b-right-vacancies sticky">
+			<h4>Поиск по вакансиям</h4>
+		</div>
+	</div>
+</div>
+
+<? include "footer.php"; ?>
